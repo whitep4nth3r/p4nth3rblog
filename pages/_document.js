b8dcece47b112a81aed72f4b29f9599767b684ad
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-02-21 15:35:51 +0000

    :sparkles: Add GA

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,24 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GTAG}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
