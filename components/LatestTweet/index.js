d11846096c7f619f421e6c7832994ce97711dafc
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-03-12 14:47:59 +0000

    :sparkles: Added tweet metrics and more

--- components/LatestTweet/index.js
+++ components/LatestTweet/index.js
@@ -3,6 +3,9 @@ import LatestTweetStyles from "@styles/LatestTweet.module.css";
 import Image from "next/image";
 import Twitter from "@components/LatestTweet/svg/Twitter";
 import LinkIcon from "@components/LatestTweet/svg/LinkIcon";
+import Heart from "@components/LatestTweet/svg/Heart";
+import Retweet from "@components/LatestTweet/svg/Retweet";
+import Reply from "@components/LatestTweet/svg/Reply";
 
 function formatUrlForShortDisplay(url) {
   return new URL(url).hostname;
@@ -15,14 +18,18 @@ function formatUrlForShortDisplay(url) {
  * - do not show reply tweets
  * - show rich url previews with images
  * - use mentions entities rather than regex for users
+ * - likes and retweets and stuff
+ *
+ * IN PROGERSS:
+ * - video media
+ *
  *
  * TODO:
+ * - i.e. youtube link
  * - Link component to my Twitter profile
  * - check gifs
  * - check mark (isaud_)
- * - video type preview image url
  * - other entities?
- * - links and retweets and stuff
  */
 
 export function processTweet(data) {
@@ -31,30 +38,35 @@ export function processTweet(data) {
 
   // remove the url string which is replaced by a rich
   // preview in processUrls
-  entities.urls.forEach((urlEntity) => {
-    _text = _text.replace(urlEntity.url, "");
-  });
+  if (entities.urls) {
+    entities.urls.forEach((urlEntity) => {
+      _text = _text.replace(urlEntity.url, "");
+    });
+  }
 
   // replace @mentions with anchor links
   // warning - probably use the start and end of the mention
   // to properly replace the text with the link
-  entities.mentions.forEach((mention) => {
-    _text = _text.replace(
-      `@${mention.username}`,
-      `<a href="https://twitter.com/${mention.username}" target="_blank">@${mention.username}</a>`,
-    );
-  });
+  if (entities.mentions) {
+    entities.mentions.forEach((mention) => {
+      _text = _text.replace(
+        `@${mention.username}`,
+        `<a href="https://twitter.com/${mention.username}" target="_blank">@${mention.username}</a>`,
+      );
+    });
+  }
 
   return parse(_text);
 }
 
-// not sure with the new query whether this will work
 function processMedia(includes) {
   // check for media type = photo
-
+  console.log(includes.media);
   const media = includes.media
     .map((media) => {
       switch (media.type) {
+        case "video":
+          return <div>THIS IS A VIDEO</div>;
         case "photo":
           return (
             <Image
@@ -78,35 +90,76 @@ function processMedia(includes) {
 function processUrls(urls) {
   const processed = urls
     .map((url) => {
-      return (
-        <a
-          href={url.expanded_url}
-          target="_blank"
-          className={LatestTweetStyles.urlPreview}
-        >
-          <div className={LatestTweetStyles.urlPreview__imgContainer}>
-            <Image alt={url.title} src={url.images[0].url} layout="fill" />
-          </div>
-          <div className={LatestTweetStyles.urlPreview__inner}>
-            <p className={LatestTweetStyles.urlPreview__title}>{url.title}</p>
-            <p className={LatestTweetStyles.urlPreview__description}>
-              {url.description}
-            </p>
-            <div className={LatestTweetStyles.urlPreview__shortUrlDisplay}>
-              <span className={LatestTweetStyles.urlPreview__linkIconContainer}>
-                <LinkIcon />
-              </span>
-              <p className={LatestTweetStyles.urlPreview__shortUrlDisplay__url}>
-                {formatUrlForShortDisplay(url.expanded_url)}
-              </p>
+      // at the moment, we check for url[images] as currently
+      // a single image is also a url...
+      if (url.images) {
+        return (
+          <a
+            href={url.expanded_url}
+            target="_blank"
+            className={LatestTweetStyles.urlPreview}
+          >
+            <div className={LatestTweetStyles.urlPreview__imgContainer}>
+              <Image alt={url.title} src={url.images[0].url} layout="fill" />
             </div>
-          </div>
-        </a>
-      );
+            <div className={LatestTweetStyles.urlPreview__inner}>
+              <p className={LatestTweetStyles.urlPreview__title}>{url.title}</p>
+              <p className={LatestTweetStyles.urlPreview__description}>
+                {url.description}
+              </p>
+              <div className={LatestTweetStyles.urlPreview__shortUrlDisplay}>
+                <span
+                  className={LatestTweetStyles.urlPreview__linkIconContainer}
+                >
+                  <LinkIcon />
+                </span>
+                <p
+                  className={LatestTweetStyles.urlPreview__shortUrlDisplay__url}
+                >
+                  {formatUrlForShortDisplay(url.expanded_url)}
+                </p>
+              </div>
+            </div>
+          </a>
+        );
+      } else {
+        return null;
+      }
     })
     .pop();
 
   return processed;
+}
+
+function processTweetMetrics(metrics) {
+  return (
+    <div className={LatestTweetStyles.metrics}>
+      <div className={LatestTweetStyles.metrics__block}>
+        <span className={LatestTweetStyles.metrics__iconContainer}>
+          <Reply />
+        </span>
+        <p className={LatestTweetStyles.metrics__metric}>
+          {metrics.reply_count}
+        </p>
+      </div>
+      <div className={LatestTweetStyles.metrics__block}>
+        <span className={LatestTweetStyles.metrics__iconContainer}>
+          <Retweet />
+        </span>
+        <p className={LatestTweetStyles.metrics__metric}>
+          {metrics.retweet_count + metrics.quote_count}
+        </p>
+      </div>
+      <div className={LatestTweetStyles.metrics__block}>
+        <span className={LatestTweetStyles.metrics__iconContainer}>
+          <Heart />
+        </span>
+        <p className={LatestTweetStyles.metrics__metric}>
+          {metrics.like_count}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default function LatestTweet({ latestTweet }) {
@@ -116,8 +169,6 @@ export default function LatestTweet({ latestTweet }) {
   if (tweet.data.referenced_tweets) {
     return null;
   }
-
-  console.log(tweet.data.text);
 
   return (
     <div className={LatestTweetStyles.container}>
@@ -142,6 +193,7 @@ export default function LatestTweet({ latestTweet }) {
       <p className={LatestTweetStyles.tweetText}>{processTweet(tweet.data)}</p>
       {tweet.includes && processMedia(tweet.includes)}
       {tweet.data.entities.urls && processUrls(tweet.data.entities.urls)}
+      {processTweetMetrics(tweet.data.public_metrics)}
     </div>
   );
 }
