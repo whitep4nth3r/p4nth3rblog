ed6412b9c553a8d560ef6089dc1020c2d3204e7c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-04-18 19:58:15 +0100

    :sparkles: Add back YT

--- components/Metrics/index.js
+++ components/Metrics/index.js
@@ -17,8 +17,8 @@ export default function Metrics() {
       <DevToViews />
       <GitHubFollowers />
       <GitHubStars />
-      {/* <YouTubeSubs /> */}
-      {/* <YouTubeViews /> */}
+      <YouTubeSubs />
+      <YouTubeViews />
     </div>
   );
 }
