4f2d77dc073c02a83e62ae5e75c2e31d5c186012
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-05-25 14:37:29 +0100

    :art: Increased query limit due to reduced complexity

--- scripts/build-search.js
+++ scripts/build-search.js
@@ -5,21 +5,18 @@ const fetch = require("node-fetch");
   dotenv.config();
 
   async function callContentful(query) {
-    const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
-
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    };
-
     try {
-      const data = await fetch(fetchUrl, fetchOptions).then((response) =>
-        response.json(),
-      );
+      const data = await fetch(
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ query }),
+        },
+      ).then((response) => response.json());
       return data;
     } catch (error) {
       throw new Error("Could not fetch data from Contentful!");
@@ -27,12 +24,13 @@ const fetch = require("node-fetch");
   }
 
   async function getPaginated(page) {
-    const queryLimit = 10;
+    const queryLimit = 100;
     const skipMultiplier = page === 1 ? 0 : page - 1;
     const skip = skipMultiplier > 0 ? queryLimit * skipMultiplier : 0;
 
     const query = `{
         blogPostCollection(limit: ${queryLimit}, skip: ${skip}, order: date_DESC) {
+          total
           items {
             sys {
               id
