d44d55cc41fff2cc04aa41793959a91ef72d27a8
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-06-15 15:49:58 +0100

    :art: Trying more custom search things

--- components/Search/index.js
+++ components/Search/index.js
@@ -3,95 +3,85 @@ import { useState, useEffect } from "react";
 import {
   InstantSearch,
   // SearchBox,
-  Hits,
+  // Hits,
   connectSearchBox,
+  connectHits,
 } from "react-instantsearch-dom";
 import Topics from "@components/Topics";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import ReactMarkdownRenderers from "@utils/ReactMarkdownRenderers";
 import PublishedDateAndReadingTime from "@components/Post/PublishedDateAndReadingTime";
-/**
- * Styles are in main.styles.json
- */
 
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
   process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
 );
 
+const Hits = ({ currentRefinement, hits }) => {
+  // this is always undefined
+  console.log(currentRefinement);
+  return (
+    <div className="ais-Hits">
+      {hits && (
+        <ol className="ais-Hits-list">
+          {hits.map((hit) => (
+            <li key={hit.objectID} className="ais-Hits-item">
+              <PublishedDateAndReadingTime
+                date={hit.date}
+                readingTime={hit.readingTime}
+              />
+              <Link href={`/blog/${hit.slug}`}>
+                <a className="ais-Hits-link">{hit.title}</a>
+              </Link>
+              <Topics topics={hit.topics} />
+              <div className="ais-Hits-excerpt">
+                <ReactMarkdown
+                  children={hit.excerpt}
+                  renderers={ReactMarkdownRenderers(hit.excerpt)}
+                />
+              </div>
+            </li>
+          ))}
+        </ol>
+      )}
+    </div>
+  );
+};
+
+const CustomHits = connectHits(Hits);
+
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
-  <form noValidate action="" role="search">
+  <form noValidate action="" role="search" className="ais-SearchBox-form">
+    <label className="ais-SearchLabel" htmlFor="algolia_search">
+      Search articles
+    </label>
     <input
+      className="ais-SearchBox-input"
+      id="algolia_search"
       type="search"
-      value={currentRefinement}
-      onChange={(event) => refine(event.currentTarget.value)}
+      placeholder="javascript tutorials"
+      onChange={(event) => {
+        event.currentTarget.value.split("").length > 2 &&
+          refine(event.currentTarget.value);
+      }}
     />
-    <button onClick={() => refine("")}>Reset query</button>
-    {isSearchStalled ? "My search is stalled" : ""}
+    <button className="ais-SearchBox-reset" onClick={() => refine("")}>
+      Clear results{" "}
+    </button>
   </form>
 );
 
 const CustomSearchBox = connectSearchBox(SearchBox);
 
-const Hit = (props) => {
-  return (
-    <>
-      <PublishedDateAndReadingTime
-        date={props.hit.date}
-        readingTime={props.hit.readingTime}
-      />
-      <Link href={`/blog/${props.hit.slug}`}>
-        <a className="ais-Hits-link">{props.hit.title}</a>
-      </Link>
-      <Topics topics={props.hit.topics} />
-      <div className="ais-Hits-excerpt">
-        <ReactMarkdown
-          children={props.hit.excerpt}
-          renderers={ReactMarkdownRenderers(props.hit.excerpt)}
-        />
-      </div>
-    </>
-  );
-};
-
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    /**
-     * To add a form label for a11y appropriately
-     */
-    const searchInput = document.querySelector(".ais-SearchBox-input");
-    searchInput.id = "algolia_search";
-  });
-
-  function searchBoxOnChange(value) {
-    setSearchTerm(value);
-  }
-
   return (
     <>
       {searchClient && (
         <div className="ais-SearchHolder">
-          <label className="ais-SearchLabel" htmlFor="algolia_search">
-            Search articles
-          </label>
           <InstantSearch searchClient={searchClient} indexName="p4nth3rblog">
             <CustomSearchBox />
-            {/* <SearchBox
-              onChange={(e) => searchBoxOnChange(e.target.value)}
-              translations={{
-                submitTitle: "Submit search",
-                resetTitle: "Clear search",
-                placeholder: "javascript tutorial",
-              }}
-              onReset={() => {
-                searchBoxOnChange("");
-              }}
-              reset="Clear results"
-            /> */}
-            {searchTerm.length >= 3 && <Hits hitComponent={Hit} />}
+            <CustomHits />
           </InstantSearch>
         </div>
       )}
