de37b9e6743a0d7c20ee5411a950819b573c066d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-06-16 10:29:32 +0100

    :sparkles: Show a message when no search results

--- components/Search/CustomHits.js
+++ components/Search/CustomHits.js
@@ -7,22 +7,25 @@ function Hits(props) {
   const validQuery = searchState.query?.length >= 3;
 
   return (
-    <>
-      {searchResults?.hits && validQuery && (
-        <div className={Styles.instantSearch__hits}>
-          <ol className={Styles.instantSearch__hitsList}>
-            {searchResults.hits.map((hit) => (
-              <li
-                key={hit.objectID}
-                className={Styles.instantSearch__hitsListItem}
-              >
-                <RecentPost post={hit} />
-              </li>
-            ))}
-          </ol>
-        </div>
+    <div className={Styles.instantSearch__hits}>
+      {searchResults?.hits.length === 0 && validQuery && (
+        <p className={Styles.instantSearch__noResults}>
+          Aw snap! No search results were found. Please try a different search.
+        </p>
       )}
-    </>
+      {searchResults?.hits.length > 0 && validQuery && (
+        <ol className={Styles.instantSearch__hitsList}>
+          {searchResults.hits.map((hit) => (
+            <li
+              key={hit.objectID}
+              className={Styles.instantSearch__hitsListItem}
+            >
+              <RecentPost post={hit} />
+            </li>
+          ))}
+        </ol>
+      )}
+    </div>
   );
 }
 
