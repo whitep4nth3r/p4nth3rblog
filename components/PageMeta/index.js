6e57f200c114cf0cc133392f9c3798c1c3dca1d3
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-09-28 20:06:27 +0100

    :ambulance: Remove sticky nav on small screens

--- components/PageMeta/index.js
+++ components/PageMeta/index.js
@@ -73,7 +73,7 @@ export default function PageMeta(props) {
       />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0f111a" />
-      <meta name="msapplication-TileColor" content="#b91d47" />
+      <meta name="msapplication-TileColor" content="#0f111a" />
       <meta name="theme-color" content="#f11012" />
     </Head>
   );
