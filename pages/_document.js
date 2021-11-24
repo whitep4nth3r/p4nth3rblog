26c5594ef0a93928903a5226feddea0051a0a70d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-11-24 13:36:53 +0000

    :ambulance: Remove unnecessary code

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=optional"
             rel="stylesheet"
           />
           <script
