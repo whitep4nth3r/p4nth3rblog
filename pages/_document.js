9c4c55d0438c72531485b54f01e8704b7d01d794
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-11-24 13:43:27 +0000

    :ambulance: Swap font loading strategy

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
           <script
