d0a6372a0ea04bba4201edad20f2f4eff6db2025
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-11-24 13:24:27 +0000

    :fire: Testing no google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
-          />
+          /> */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
