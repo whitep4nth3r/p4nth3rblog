c17ea3586a91bf3c99dfc6b3529354893d8f1ae8
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-14 09:53:42 +0000

    Revert loader changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   images: {
-    loader: "imgix",
-    path: "https://images.ctfassets.net",
     domains: ["images.ctfassets.net"],
     formats: ["image/avif", "image/webp"],
   },
