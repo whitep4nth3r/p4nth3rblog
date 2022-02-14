77b0c1c96e8b2933770cf224663e4fa6f16738db
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-14 09:42:31 +0000

    Add protocol to path for image loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     loader: "imgix",
-    path: "images.ctfassets.net",
+    path: "https://images.ctfassets.net",
     domains: ["images.ctfassets.net"],
     formats: ["image/avif", "image/webp"],
   },
